9a975202db6b41c338f26dd05ef22e00df750ade
[pudasaineeamrita7@gmail.com] <pudasaineeamrita7@gmail.com>
2025-06-21 07:28:16 +0545

    DSA, RegEx, Thread

--- week7/LambdaDemo.java
+++ week7/LambdaDemo.java
@@ -1,9 +1,17 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 
+interface Calculate{
+    void addition(int a, int b);
+}
+
 public class LambdaDemo{
     public static void main(String[] args) {
         ArrayList<Integer> numbers=new ArrayList<>(Arrays.asList(1,2,3,4,5));
         numbers.forEach(n-> System.out.println(n));
+
+        Calculate calculate=(int a, int b)-> System.out.println(a+b);
+        calculate.addition(3, 4);
+
     }
 }
