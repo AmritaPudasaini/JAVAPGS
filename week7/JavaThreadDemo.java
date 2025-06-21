351b369711ba5441e507375c1874f006d25bef16
[pudasaineeamrita7@gmail.com] <pudasaineeamrita7@gmail.com>
2025-06-21 07:07:53 +0545

    DSA, RegEx, Thread

--- week7/JavaThreadDemo.java
+++ week7/JavaThreadDemo.java
@@ -3,24 +3,43 @@ class FirstThread extends Thread{
     public void run(){
         for (int i=1;i<=100;i++){
             System.out.println("hii");
+            try{
+                Thread.sleep(100);
+            } catch (InterruptedException e){
+                throw new RuntimeException(e);
+            }
         }
     }
 }
-
 class SecThread extends Thread{
     @Override
     public void run(){
         for (int i=1;i<=100;i++){
             System.out.println("Hello");
+            try{
+                Thread.sleep(100);
+            } catch (InterruptedException e){
+                throw new RuntimeException(e);
+            }
         }
+    }
+}
+
+class ThirdThread implements Runnable{
+    @Override
+    public void run(){
+        System.out.println("third thread");
     }
 }
 public class JavaThreadDemo{
     public static void main(String[] args) {
         FirstThread t1=new FirstThread();
         SecThread t2=new SecThread();
+        ThirdThread tt=new ThirdThread();
+        Thread t3=new Thread(tt);
 
         t1.start();
         t2.start();
+        t3.start();
     }
 }
